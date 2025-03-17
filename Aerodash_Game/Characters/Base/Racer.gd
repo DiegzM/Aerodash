d85c058df55d530eceb8d2e570721753c2ae977f
DiegzM <147036619+DiegzM@users.noreply.github.com>
2025-03-16 21:33:56 -0700

    Fix camera position movement,  Add roll

--- Aerodash_Game/Characters/Base/Racer.gd
+++ Aerodash_Game/Characters/Base/Racer.gd
@@ -2,21 +2,78 @@ class_name Racer
 
 extends RigidBody3D
 
+# -------------------------------------
+# VARIABLES
+# -------------------------------------
+
+# Export Variables
+
 @export var custom_vehicle: PackedScene
 @export var randomize_vehicle_selection: bool = true
 
+# Physics Settings
+
+const ACCELERATION = 380.0
+const MAX_SPEED = 60.0
+const BOOST_SPEED = 100.0
+const BOOST_MAX_TIME = 7.0
+const BOOST_RECHARGE_RATE = 1.4
+const MOVEMENT_DAMPING = 0.95
+const ROTATION_SMOOTHNESS = 0.05
+const LERP_VELOCITY = 0.05
+const MAX_DOWNWARD_FACTOR = 1.6  # Speed increase when facing down
+const MAX_UPWARD_FACTOR = 0.9    # Speed reduction when facing up
+const ROLL_SPEED = 5.0
+const ROLL_ACCELERATION = 10.0
+
+# Onready Variables
+
+enum RacerState {
+	NORMAL,
+	DEAD,
+	RESPAWNING,
+	DEACTIVATED
+}
+
+@onready var roll_direction = 0
+@onready var current_roll_speed = 0.0
+
+@onready var is_boosting = false
+@onready var is_dashing = false
+@onready var current_state = RacerState.NORMAL
+
 @onready var collision_shape = $CollisionShape3D
-
 @onready var target_pivot = $TargetPivot
-
 @onready var vehicle_scenes: Array[PackedScene] = []
 @onready var vehicle = $Vehicle
 
+@onready var movement_direction = Vector3.ZERO
+@onready var target_basis = target_pivot.global_transform.basis
+
+# -------------------------------------
+# INITIALIZATION
+# -------------------------------------
 func _ready() -> void:
-	#Load Vehicle
+	# Load Vehicle
 	load_vehicle_scenes()
 	load_vehicle()
+
+# -------------------------------------
+# UPDATE
+# -------------------------------------
+func _physics_process(delta: float) -> void:
+	match current_state:
+		RacerState.NORMAL:
+			process_movement(delta)
+			process_rotation(delta)
+		RacerState.DEAD:
+			pass
+		RacerState.RESPAWNING:
+			pass
 	
+# -------------------------------------
+# VEHICLE MANAGEMENT
+# -------------------------------------
 func load_vehicle_scenes():
 	var dir = DirAccess.open("res://Assets/Vehicles/CustomVehicles")
 	if dir:
@@ -33,11 +90,9 @@ func load_vehicle_scenes():
 		dir.list_dir_end()
 		
 func load_vehicle():
-	# Skip if we should keep the default vehicle
 	if (not custom_vehicle and not randomize_vehicle_selection) or vehicle_scenes.size() == 0:
 		return
 	
-	# Determine which vehicle to load
 	var vehicle_scene: PackedScene
 	if custom_vehicle:
 		vehicle_scene = custom_vehicle
@@ -45,7 +100,6 @@ func load_vehicle():
 		var random_index = randi() % vehicle_scenes.size()
 		vehicle_scene = vehicle_scenes[random_index]
 	
-	# Replace vehicle
 	if is_instance_valid(vehicle):
 		vehicle.queue_free()
 	
@@ -53,8 +107,76 @@ func load_vehicle():
 	new_vehicle.name = "Vehicle"
 	add_child(new_vehicle)
 	
-	# Update reference to the new vehicle
 	vehicle = new_vehicle
 
-func _process(delta: float):
-	pass
+# -------------------------------------
+# MOVEMENT
+# -------------------------------------
+func process_movement(delta: float) -> void:
+	var current_max_speed = MAX_SPEED
+	var current_acceleration = ACCELERATION
+	
+	angular_velocity = Vector3.ZERO
+	
+	if is_boosting:
+		current_max_speed = BOOST_SPEED
+		current_acceleration = ACCELERATION * (BOOST_SPEED / MAX_SPEED)
+	
+	var local_force = movement_direction * current_acceleration * (delta * 60)
+	
+	if movement_direction.x == 0:
+		linear_velocity.x *= pow(MOVEMENT_DAMPING, delta * 60)
+	if movement_direction.y == 0:
+		linear_velocity.y *= pow(MOVEMENT_DAMPING, delta * 60)
+	if movement_direction.z == 0:
+		linear_velocity.z *= pow(MOVEMENT_DAMPING, delta * 60)
+	
+	var speed = linear_velocity.length()
+	if speed > current_max_speed:
+		var target_velocity = linear_velocity.normalized() * current_max_speed
+		linear_velocity = lerp(linear_velocity, target_velocity, LERP_VELOCITY)
+	
+	apply_force(local_force)
+	
+
+# -------------------------------------
+# ROTATION
+# -------------------------------------
+func process_rotation(delta: float) -> void:
+	
+	angular_velocity = Vector3.ZERO
+	
+	var preserved_pivot_transform = target_pivot.global_transform
+	var current_rotation = global_rotation
+	var target_rotation = target_pivot.global_rotation
+	
+	current_rotation.x = lerp_angle(current_rotation.x, target_rotation.x, ROTATION_SMOOTHNESS)
+	current_rotation.y = lerp_angle(current_rotation.y, target_rotation.y, ROTATION_SMOOTHNESS)
+	current_rotation.z = lerp_angle(current_rotation.z, target_rotation.z, ROTATION_SMOOTHNESS)
+	
+	global_rotation = current_rotation
+	
+	target_pivot.global_transform = preserved_pivot_transform
+	
+	# ROLL
+	var target_roll_speed = ROLL_SPEED * -roll_direction * delta
+	current_roll_speed = lerp(current_roll_speed, target_roll_speed, ROLL_ACCELERATION * delta)
+	
+	if abs(current_roll_speed) > 0.001:
+		target_pivot.rotate_object_local(Vector3.FORWARD, current_roll_speed)
+	
+# -------------------------------------
+# PUBLIC SET FUNCTIONS
+# -------------------------------------
+
+func set_state(state: RacerState) -> void:
+	current_state = state
+	
+func set_movement_direction(direction: Vector3) -> void:
+	movement_direction = direction
+	
+func set_boost(enabled: bool) -> void:
+	is_boosting = enabled
+
+func set_roll(direction) -> void:
+	roll_direction = direction
--- Aerodash_Game/Characters/Player/Player.gd
+++ Aerodash_Game/Characters/Player/Player.gd
@@ -1,12 +1,129 @@
 extends Racer
 
-# Called when the node enters the scene tree for the first time.
+# -------------------------------------
+# VARIABLES
+# -------------------------------------
+
+# Settings
+
+const MOUSE_SENSITIVITY= 0.3
+
+const FCAM_MIN_FOV = 85
+const FCAM_MAX_FOV = 140
+const BCAM_MIN_FOV = 85
+const BCAM_MAX_FOV = 110
+const FOV_SMOOTHNESS = 10
+const CAM_POSITION_DAMPING = 0.2
+const CAM_OFFSET_STRENGTH = 0.008
+
+# Onready Variables
+
+@onready var fcam = target_pivot.get_node("FCamera")
+@onready var bcam = target_pivot.get_node("BCamera")
+@onready var fcam_base_transform = fcam.transform
+@onready var bcam_base_transform = fcam.transform
+@onready var velocity_offset = Vector3.ZERO
+
+@onready var mouse_button_pressed = false
+@onready var mouse_delta = Vector2.ZERO
+
+# -------------------------------------
+# INITIALIZE
+# -------------------------------------
 func _ready() -> void:
 	super._ready()
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	super._process(delta)
-	pass
 	
+	# Set the GUI to lock mouse in place
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# -------------------------------------
+# INPUT
+# -------------------------------------
+func _input(event) -> void:
+	if event is InputEventMouseMotion:
+		mouse_delta = event.relative
+	if event is InputEventMouseButton:
+		mouse_button_pressed = event.is_pressed()
+
+# -------------------------------------
+# UPDATE
+# -------------------------------------
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+	
+	match current_state:
+		RacerState.NORMAL:
+			process_keyboard_input()
+			process_mouse_input()
+	
+	process_cameras(delta)
+		
+	
+# -------------------------------------
+# INPUTS
+# -------------------------------------
+func process_keyboard_input() -> void:
+	var direction = Vector3.ZERO
+	
+	# Movements
+	if Input.is_action_pressed("move_forward"):
+		direction += target_pivot.global_transform.basis.z
+	if Input.is_action_pressed("move_backward"):
+		direction -= target_pivot.global_transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		direction += target_pivot.global_transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		direction -= target_pivot.global_transform.basis.x
+	if Input.is_action_pressed("move_up"):
+		direction += target_pivot.global_transform.basis.y
+	if Input.is_action_pressed("move_down"):
+		direction -= target_pivot.global_transform.basis.y
+	
+	# Roll
+	if Input.is_action_pressed("roll_right") and not Input.is_action_pressed("roll_left"):
+		set_roll(1)
+	elif Input.is_action_pressed("roll_left") and not Input.is_action_pressed("roll_right"):
+		set_roll(-1)
+	elif not roll_direction == 0:
+		set_roll(0)
+
+	set_movement_direction(direction.normalized())
+
+func process_mouse_input() -> void:
+	var pitch_delta = deg_to_rad(mouse_delta.y * MOUSE_SENSITIVITY)
+	var yaw_delta = deg_to_rad(-mouse_delta.x * MOUSE_SENSITIVITY)
+
+	target_pivot.rotate_object_local(Vector3.UP, yaw_delta)
+	target_pivot.rotate_object_local(Vector3.RIGHT, pitch_delta)
+	
+	# Reset mouse delta
+	mouse_delta = Vector2.ZERO
+	
+	# Boost
+	if Input.is_action_just_pressed("boost"):
+		set_boost(true)
+	elif Input.is_action_just_released("boost"):
+		set_boost(false)
+		
+
+# -------------------------------------
+# CAMERA EFFECTS
+# -------------------------------------
+func process_cameras(delta) -> void:
+	
+	# FOV
+	var fcam_target_fov = lerp(FCAM_MIN_FOV, FCAM_MAX_FOV, clamp(linear_velocity.length() / BOOST_SPEED, 0, 1))
+	var bcam_target_fov = lerp(BCAM_MIN_FOV, BCAM_MAX_FOV, clamp(linear_velocity.length() / BOOST_SPEED, 0, 1))
+	
+	fcam.fov = lerp(fcam.fov, fcam_target_fov, FOV_SMOOTHNESS * delta)
+	bcam.fov = lerp(bcam.fov, bcam_target_fov, FOV_SMOOTHNESS * delta)
+	
+	# Position Damping
+	var target_offset = global_transform.basis.inverse() * -linear_velocity * CAM_OFFSET_STRENGTH
+	
+	velocity_offset = velocity_offset.lerp(target_offset, CAM_POSITION_DAMPING)
+	var fcam_target_position = fcam_base_transform.origin + velocity_offset
+	var bcam_target_position = bcam_base_transform.origin + velocity_offset
+	
+	fcam.transform.origin = fcam_target_position
+	bcam.transform.origin = bcam_target_position
